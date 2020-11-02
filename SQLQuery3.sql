a16cfff8bde33afc3088fe6fdfe4dba062ece46b
FabGtte <41702805+FabGtte@users.noreply.github.com>
2020-11-02 14:46:58 +0100

    Delete SQLQuery3.sql

--- SQLQuery3.sql
+++
@@ -1,6 +0,0 @@
-USE QCardinalites;
-
-SELECT Products.product_name AS Nom_Produits, dbo.Stock.Stock_quantity 
-FROM Products 
-JOIN dbo.Stock ON dbo.Products.Products_ID = dbo.Stock.FK_Products_ID
-WHERE  FK_Category_ID = 1;
